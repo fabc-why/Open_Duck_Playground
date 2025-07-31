4995011a9fdf2eb3716498f5d7f613b5d3913946
apirrone <awanegenebayo@gmail.com>
2025-07-31 23:05:13 +0200

    cmd ranges

--- playground/open_duck_mini_v2/ref_motion_viewer.py
+++ playground/open_duck_mini_v2/ref_motion_viewer.py
@@ -15,9 +15,9 @@ from playground.common.poly_reference_motion_numpy import PolyReferenceMotion
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 SCENE_PATH = f"{SCRIPT_PATH}/xmls"
 
-COMMANDS_RANGE_X = [-0.15, 0.15]
+COMMANDS_RANGE_X = [-0.25, 0.3]
 COMMANDS_RANGE_Y = [-0.2, 0.2]
-COMMANDS_RANGE_THETA = [-1.0, 1.0]  # [-1.0, 1.0]
+COMMANDS_RANGE_THETA = [-1.5, 1.5]  # [-1.0, 1.0]
 
 available_scenes = []
 if os.path.isdir(SCENE_PATH):
