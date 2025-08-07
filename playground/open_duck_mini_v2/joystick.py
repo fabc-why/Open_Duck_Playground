137ddbb56dfd897a32f738438c0022760c38152c
apirrone <awanegenebayo@gmail.com>
2025-08-07 12:49:55 +0200

    sample zero command at 0.1 prob, and set imitation_i=0 if command is zero

--- playground/open_duck_mini_v2/joystick.py
+++ playground/open_duck_mini_v2/joystick.py
@@ -325,11 +325,11 @@ class Joystick(open_duck_mini_v2_base.OpenDuckMiniV2Env):
     def step(self, state: mjx_env.State, action: jax.Array) -> mjx_env.State:
 
         if USE_IMITATION_REWARD:
-            if np.linalg.norm(state.info["command"][:3]) != 0:
+            if jp.linalg.norm(state.info["command"][:3]) != 0:
                 state.info["imitation_i"] += 1
             else:
                 state.info["imitation_i"] = 0
-                
+
             state.info["imitation_i"] = (
                 state.info["imitation_i"] % self.PRM.nb_steps_in_period
             )  # not critical, is already moduloed in get_reference_motion
