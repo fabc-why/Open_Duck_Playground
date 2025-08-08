6344f328a682ee6d5d03c97e8c2bdf1cea15f44d
apirrone <awanegenebayo@gmail.com>
2025-08-08 17:35:44 +0200

    checking that this simplification still works

--- playground/open_duck_mini_v2/joystick.py
+++ playground/open_duck_mini_v2/joystick.py
@@ -65,9 +65,10 @@ def default_config() -> config_dict.ConfigDict:
             imu_min_delay=0,  # env steps
             imu_max_delay=3,  # env steps
             scales=config_dict.create(
-                hip_pos=0.03,  # rad, for each hip joint
-                knee_pos=0.05,  # rad, for each knee joint
-                ankle_pos=0.08,  # rad, for each ankle joint
+                # hip_pos=0.03,  # rad, for each hip joint
+                # knee_pos=0.05,  # rad, for each knee joint
+                # ankle_pos=0.08,  # rad, for each ankle joint
+                pos=0.05,  # rad, for each joint
                 joint_vel=2.5,  # rad/s # Was 1.5
                 gravity=0.1,
                 linvel=0.1,
@@ -80,7 +81,7 @@ def default_config() -> config_dict.ConfigDict:
                 tracking_lin_vel=2.5,
                 tracking_ang_vel=6.0,
                 torques=-1.0e-3,
-                action_rate=-0.5,  # was -2.0, made stable policy
+                action_rate=-1.5,  # was -2.0, made stable policy
                 stand_still=0.0,  # was -1.0 TODO try to relax this a bit ?
                 alive=20.0,
                 imitation=1.0,
@@ -91,7 +92,7 @@ def default_config() -> config_dict.ConfigDict:
         push_config=config_dict.create(
             enable=True,
             interval_range=[3.0, 7.0],
-            magnitude_range=[0.3, 0.4],
+            magnitude_range=[0.3, 1.0],
         ),
         lin_vel_x=[-0.15, 0.15],
         lin_vel_y=[-0.2, 0.2],
@@ -140,23 +141,6 @@ class Joystick(open_duck_mini_v2_base.OpenDuckMiniV2Env):
         self._soft_lowers = c - 0.5 * r * self._config.soft_joint_pos_limit_factor
         self._soft_uppers = c + 0.5 * r * self._config.soft_joint_pos_limit_factor
 
-        # weights for computing the cost of each joints compared to a reference pose
-        self._weights = jp.array(
-            [
-                1.0,
-                1.0,
-                0.01,
-                0.01,
-                1.0,  # left leg.
-                # 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, #head
-                1.0,
-                1.0,
-                0.01,
-                0.01,
-                1.0,  # right leg.
-            ]
-        )
-
         self._njoints = self._mj_model.njnt  # number of joints
         self._actuators = self._mj_model.nu  # number of actuators
 
@@ -183,23 +167,25 @@ class Joystick(open_duck_mini_v2_base.OpenDuckMiniV2Env):
         self._foot_linvel_sensor_adr = jp.array(foot_linvel_sensor_adr)
 
         # # noise in the simu?
-        qpos_noise_scale = np.zeros(self._actuators)
+        # qpos_noise_scale = np.zeros(self._actuators)
 
-        hip_ids = [
-            idx for idx, j in enumerate(constants.JOINTS_ORDER_NO_HEAD) if "_hip" in j
-        ]
-        knee_ids = [
-            idx for idx, j in enumerate(constants.JOINTS_ORDER_NO_HEAD) if "_knee" in j
-        ]
-        ankle_ids = [
-            idx for idx, j in enumerate(constants.JOINTS_ORDER_NO_HEAD) if "_ankle" in j
-        ]
+        # hip_ids = [
+        #     idx for idx, j in enumerate(constants.JOINTS_ORDER_NO_HEAD) if "_hip" in j
+        # ]
+        # knee_ids = [
+        #     idx for idx, j in enumerate(constants.JOINTS_ORDER_NO_HEAD) if "_knee" in j
+        # ]
+        # ankle_ids = [
+        #     idx for idx, j in enumerate(constants.JOINTS_ORDER_NO_HEAD) if "_ankle" in j
+        # ]
 
-        qpos_noise_scale[hip_ids] = self._config.noise_config.scales.hip_pos
-        qpos_noise_scale[knee_ids] = self._config.noise_config.scales.knee_pos
-        qpos_noise_scale[ankle_ids] = self._config.noise_config.scales.ankle_pos
-        # qpos_noise_scale[faa_ids] = self._config.noise_config.scales.faa_pos
-        self._qpos_noise_scale = jp.array(qpos_noise_scale)
+        # qpos_noise_scale[hip_ids] = self._config.noise_config.scales.hip_pos
+        # qpos_noise_scale[knee_ids] = self._config.noise_config.scales.knee_pos
+        # qpos_noise_scale[ankle_ids] = self._config.noise_config.scales.ankle_pos
+        # # qpos_noise_scale[faa_ids] = self._config.noise_config.scales.faa_pos
+
+        # self._qpos_noise_scale = jp.array(qpos_noise_scale)
+        self._qpos_noise_scale = jp.ones(self._actuators) * self._config.noise_config.scales.pos
 
         # self.action_filter = LowPassActionFilter(
         #     1 / self._config.ctrl_dt, cutoff_frequency=37.5
