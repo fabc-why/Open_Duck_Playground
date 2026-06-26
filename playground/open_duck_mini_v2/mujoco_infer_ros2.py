a120046382a24d4256791af9f778ad03e0be5929
Tetsuya YAMADA <k725562@kansai-u.ac.jp>
2026-06-26 11:00:52 +0900

    とりあえず、頭のカメラの画像を取得できるようになった。

---
+++ playground/open_duck_mini_v2/mujoco_infer_ros2.py
@@ -0,0 +1,250 @@
+import mujoco
+import pickle
+import numpy as np
+import mujoco
+import mujoco.viewer
+import time
+import argparse
+from playground.common.onnx_infer import OnnxInfer
+from playground.common.poly_reference_motion_numpy import PolyReferenceMotion
+from playground.common.utils import LowPassActionFilter
+import cv2
+
+from playground.open_duck_mini_v2.mujoco_infer_base import MJInferBase
+
+USE_MOTOR_SPEED_LIMITS = True
+
+
+class MjInfer(MJInferBase):
+    def __init__(
+        self,
+        model_path: str,
+        reference_data: str,
+        onnx_model_path: str,
+        standing: bool,
+    ):
+        super().__init__(model_path)
+
+        self.standing = standing
+        self.head_control_mode = self.standing
+
+        # Params
+        self.linearVelocityScale = 1.0
+        self.angularVelocityScale = 1.0
+        self.dof_pos_scale = 1.0
+        self.dof_vel_scale = 0.05
+        self.action_scale = 0.25
+
+        self.action_filter = LowPassActionFilter(50, cutoff_frequency=37.5)
+
+
+
+        if not self.standing:
+            self.PRM = PolyReferenceMotion(reference_data)
+
+        self.policy = OnnxInfer(onnx_model_path, awd=True)
+
+        self.COMMANDS_RANGE_X = [-0.15, 0.15]
+        self.COMMANDS_RANGE_Y = [-0.2, 0.2]
+        self.COMMANDS_RANGE_THETA = [-1.0, 1.0]  # [-1.0, 1.0]
+
+        self.NECK_PITCH_RANGE = [-0.34, 1.1]
+        self.HEAD_PITCH_RANGE = [-0.78, 0.78]
+        self.HEAD_YAW_RANGE = [-1.5, 1.5]
+        self.HEAD_ROLL_RANGE = [-0.5, 0.5]
+
+        self.last_action = np.zeros(self.num_dofs)
+        self.last_last_action = np.zeros(self.num_dofs)
+        self.last_last_last_action = np.zeros(self.num_dofs)
+        self.commands = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+        self.imitation_i = 0
+        self.imitation_phase = np.array([0, 0])
+        self.saved_obs = []
+
+        self.max_motor_velocity = 5.24  # rad/s
+
+        self.phase_frequency_factor = 1.0
+        
+        self.ros2_connected = False
+
+        self.renderer = mujoco.Renderer(self.model)
+
+        print(f"joint names: {self.joint_names}")
+        print(f"actuator names: {self.actuator_names}")
+        print(f"backlash joint names: {self.backlash_joint_names}")
+        # print(f"actual joints idx: {self.get_actual_joints_idx()}")
+
+    def get_obs(
+        self,
+        data,
+        command,  # , qvel_history, qpos_error_history, gravity_history
+    ):
+        gyro = self.get_gyro(data)
+        accelerometer = self.get_accelerometer(data)
+        accelerometer[0] += 1.3
+
+        joint_angles = self.get_actuator_joints_qpos(data.qpos)
+        joint_vel = self.get_actuator_joints_qvel(data.qvel)
+
+        contacts = self.get_feet_contacts(data)
+
+        # if not self.standing:
+        # ref = self.PRM.get_reference_motion(*command[:3], self.imitation_i)
+
+        obs = np.concatenate(
+            [
+                gyro,
+                accelerometer,
+                # gravity,
+                command,
+                joint_angles - self.default_actuator,
+                joint_vel * self.dof_vel_scale,
+                self.last_action,
+                self.last_last_action,
+                self.last_last_last_action,
+                self.motor_targets,
+                contacts,
+                # ref if not self.standing else np.array([]),
+                # [self.imitation_i]
+                self.imitation_phase,
+            ]
+        )
+
+        return obs
+
+    # no longer used, but kept for reference
+    def key_callback(self, keycode):
+        print(f"key: {keycode}")
+
+    def run(self):
+        try:
+            with mujoco.viewer.launch_passive(
+                    self.model,
+                    self.data,
+                    show_left_ui=True,
+                    show_right_ui=False,
+                    key_callback=self.key_callback,
+                ) as viewer:
+                counter = 0
+                while True:
+
+                    step_start = time.time()
+
+                    # ros connection is established, accept the commands from ROS2 topic
+                    #if self.ros2_connected:
+                    #    self.commands = self.latest_ros2_commands
+
+                    mujoco.mj_step(self.model, self.data)
+
+                    # 頭のカメラの映像
+                    self.renderer.update_scene(self.data, camera="robot_head_cam")
+                    img = self.renderer.render(width=640, height=480, depth=False)
+
+                    cv2.imshow("robot_head_cam", img)
+                    cv2.waitKey(1)
+
+                    if counter % 1000 == 0:
+                        self.commands[2] = np.random.uniform(
+                            self.COMMANDS_RANGE_THETA[0], self.COMMANDS_RANGE_THETA[1]
+                        )
+
+
+                    counter += 1
+
+                    if counter % self.decimation == 0:
+                        if not self.standing:
+                            self.imitation_i += 1.0 * self.phase_frequency_factor
+                            self.imitation_i = (
+                                self.imitation_i % self.PRM.nb_steps_in_period
+                            )
+                            # print(self.PRM.nb_steps_in_period)
+                            # exit()
+                            self.imitation_phase = np.array(
+                                [
+                                    np.cos(
+                                        self.imitation_i
+                                        / self.PRM.nb_steps_in_period
+                                        * 2
+                                        * np.pi
+                                    ),
+                                    np.sin(
+                                        self.imitation_i
+                                        / self.PRM.nb_steps_in_period
+                                        * 2
+                                        * np.pi
+                                    ),
+                                ]
+                            )
+                        obs = self.get_obs(
+                            self.data,
+                            self.commands,
+                        )
+                        self.saved_obs.append(obs)
+                        action = self.policy.infer(obs)
+
+                        # self.action_filter.push(action)
+                        # action = self.action_filter.get_filtered_action()
+
+                        self.last_last_last_action = self.last_last_action.copy()
+                        self.last_last_action = self.last_action.copy()
+                        self.last_action = action.copy()
+
+                        self.motor_targets = (
+                            self.default_actuator + action * self.action_scale
+                        )
+
+                        if USE_MOTOR_SPEED_LIMITS:
+                            self.motor_targets = np.clip(
+                                self.motor_targets,
+                                self.prev_motor_targets
+                                - self.max_motor_velocity
+                                * (self.sim_dt * self.decimation),
+                                self.prev_motor_targets
+                                + self.max_motor_velocity
+                                * (self.sim_dt * self.decimation),
+                            )
+
+                            self.prev_motor_targets = self.motor_targets.copy()
+
+                        # head_targets = self.commands[3:]
+                        # self.motor_targets[5:9] = head_targets
+                        self.data.ctrl = self.motor_targets.copy()
+
+                    viewer.sync()
+
+                    time_until_next_step = self.model.opt.timestep - (
+                        time.time() - step_start
+                    )
+                    if time_until_next_step > 0:
+                        time.sleep(time_until_next_step)
+        except KeyboardInterrupt:
+            pickle.dump(self.saved_obs, open("mujoco_saved_obs.pkl", "wb"))
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--onnx_model_path", type=str, default="checkpoints/2026_06_15_152253_151388160.onnx")
+
+    parser.add_argument(
+        "--reference_data",
+        type=str,
+        default="playground/open_duck_mini_v2/data/polynomial_coefficients.pkl",
+    )
+    parser.add_argument(
+        "--model_path",
+        type=str,
+        default="playground/open_duck_mini_v2/xmls/scene_flat_terrain.xml",
+    )
+    parser.add_argument("--standing", action="store_true", default=False)
+
+    args = parser.parse_args()
+
+    mjinfer = MjInfer(
+        args.model_path,
+        args.reference_data,
+        args.onnx_model_path,
+        args.standing
+    )
+    mjinfer.run()
